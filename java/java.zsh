da6f39e489166281cf687126f7f2ded0aa3e7bcf
vagrant <vagrant@lucid32.(none)>
2012-07-05 19:45:05 +0200

    update java aliases

--- java/java.zsh
+++ java/java.zsh
@@ -1,14 +1,20 @@
 export M2_HOME=/usr/share/maven
 
+# Set different home directories based on OS
+
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
 	export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk
+	export TOMCAT_HOME=/usr/local/tomcat
 elif [[ "$unamestr" == 'Darwin' ]]; then
 	export JAVA_HOME=`/usr/libexec/java_home`
+	export TOMCAT_HOME=/Applications/tomcat
 fi
 
-export MAVEN_OPTS="-Xms256m -Xmx1024m -XX:PermSize=1024m -XX:MaxPermSize=1024m"
+# Tomcat Options
 
+export MAVEN_OPTS="-Xms256m -Xmx1024m -XX:PermSize=1024m -XX:MaxPermSize=1024m"
+export JPDA_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 "
 export CATALINA_PID=/usr/local/tomcat/bin/catalina.pid
 
 export CATALINA_OPTS="$CATALINA_OPTS $JPDA_OPTS -Xmx1024m -XX:MaxPermSize=512m 
@@ -20,3 +26,8 @@ export CATALINA_OPTS="$CATALINA_OPTS $JPDA_OPTS -Xmx1024m -XX:MaxPermSize=512m
 -Dwbx.config.loc=file:/vagrant/props/dev.canonical.properties 
 -Djava.awt.headless=true 
 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true "
+
+# Configure aliases
+alias stc='$TOMCAT_HOME/bin/startup.sh'
+alias htc='$TOMCAT_HOME/bin/shutdown.sh'
+
