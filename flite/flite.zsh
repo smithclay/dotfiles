77913404c20bbce0b7d003ddb88eb546781ba8ba
Clay Smith <smithclay@gmail.com>
2012-07-05 15:35:03 -0700

    add aliases for node and flite-specific dev tools

---
+++ flite/flite.zsh
@@ -0,0 +1,4 @@
+alias dsr='cd $PROJECTS/server/trunk; ant deploy-syndication-resources; cd -;'
+alias dgr='cd $PROJECTS/server/trunk; ant deploy-gallery-resources; cd -;'
+alias dg='cd $PROJECTS/server/trunk; ant deploy-gallery; cd -;'
+alias ds='cd $PROJECTS/server/trunk; ant deploy-syndication; cd -;'
---
+++ node/node.zsh
@@ -0,0 +1,2 @@
+alias grunt='nocorrect grunt'
+alias npm='no correct npm'
