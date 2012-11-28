6c892fdf8870e4d226a46a5da5597c4a7bfc61a9
Clay Smith <smithclay@gmail.com>
2012-11-27 17:33:30 -0800

    add trunk directory environment variable

--- flite/flite.zsh
+++ flite/flite.zsh
@@ -2,3 +2,5 @@ alias dsr='cd $PROJECTS/server/trunk; ant deploy-syndication-resources; cd -;'
 alias dgr='cd $PROJECTS/server/trunk; ant deploy-gallery-resources; cd -;'
 alias dg='cd $PROJECTS/server/trunk; ant deploy-gallery; cd -;'
 alias ds='cd $PROJECTS/server/trunk; ant deploy-syndication; cd -;'
+
+export FLITE_TRUNK=$HOME/dev/server/trunk
