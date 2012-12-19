a2f59a5a01607dc12b0ec28366350f1e7b7fc525
Clay Smith <smithclay@gmail.com>
2012-12-18 16:19:39 -0800

    add sayeasy host

--- flite/flite.zsh
+++ flite/flite.zsh
@@ -1,7 +1,10 @@
 alias dsr='cd $PROJECTS/server/trunk; ant deploy-syndication-resources; cd -;'
 alias dgr='cd $PROJECTS/server/trunk; ant deploy-gallery-resources; cd -;'
-alias dg='cd $PROJECTS/vagrant; vagrant ssh -c "service tomcat6 stop"; cd $PROJECTS/server/trunk; ant deploy-gallery; cd $PROJECTS/vagrant; vagrant ssh -c "service tomcat6 start"; cd -;'
-alias ds='cd $PROJECTS/vagrant; vagrant ssh -c "service tomcat6 stop"; cd $PROJECTS/server/trunk; ant deploy-syndication; cd $PROJECTS/vagrant; vagrant ssh -c "service tomcat6 start"; cd -;'
-alias rtc='cd $PROJECTS/vagrant; vagrant ssh -c "service tomcat6 restart"; cd -;'
+alias dg='cd $PROJECTS/server/trunk; ant deploy-gallery; cd -;'
+alias ds='cd $PROJECTS/server/trunk; ant deploy-syndication; cd -;'
+alias htc='cd $PROJECTS/vagrant; vagrant ssh -c "/etc/init.d/tomcat6 stop"; cd -;'
+alias rtc='cd $PROJECTS/vagrant; vagrant ssh -c "/etc/init.d/tomcat6 restart"; cd -;'
 
 export FLITE_TRUNK=$HOME/dev/server/trunk
+export SAYEASY_HOST=Kennedy.local
+
