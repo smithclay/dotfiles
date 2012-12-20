a12f30752442c9ba1ad20eb7303dd3fd01c002a2
Clay Smith <smithclay@gmail.com>
2012-12-20 12:06:45 -0800

    remove FLITE_TRUNK property for vagrant

--- flite/flite.zsh
+++ flite/flite.zsh
@@ -5,6 +5,5 @@ alias ds='cd $PROJECTS/server/trunk; ant deploy-syndication; cd -;'
 alias htc='cd $PROJECTS/vagrant; vagrant ssh -c "/etc/init.d/tomcat6 stop"; cd -;'
 alias rtc='cd $PROJECTS/vagrant; vagrant ssh -c "/etc/init.d/tomcat6 restart"; cd -;'
 
-export FLITE_TRUNK=$HOME/dev/server/trunk
 export SAYEASY_HOST=Kennedy.local
 
